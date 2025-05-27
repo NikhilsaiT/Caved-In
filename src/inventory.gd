f5d7ae62065f683bc012c67a17d8b5dd8810957c
NikhilsaiT <ntanguturi2008@gmail.com>
2025-05-27 17:04:43 -0500

    lemmeCook

--- src/inventory.gd
+++ src/inventory.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	for slot in get_tree().get_nodes_in_group("slots"):
-		if(Input.is_action_just_pressed("toggle inventory")): #hides inventory when inventory is not open
+		if(Input.is_action_just_pressed("inventory")): #hides inventory when inventory is not open
 			if slot.type == 1:
 				if inventoryOn:
 					slot.visible = false
@@ -42,19 +42,15 @@ func _process(delta: float) -> void:
 				pickup(itemInHand,slot.slotID)
 				itemInHand = null
 		
-	if(Input.is_action_just_pressed("toggle inventory")): #toggles inventory when E key is pressed
+	if(Input.is_action_just_pressed("inventory")): #toggles inventory when E key is pressed
 		inventoryOn = !inventoryOn
-	
-<<<<<<< HEAD
 	#if(Input.is_action_just_pressed("right")): #DEBUG: adds 1 iron ingot to inventory (delete later)
 		#var rng = RandomNumberGenerator.new()
 		#for n in range(100):
 			#pickup(Item.new(rng.randi_range(1,4),1),0)
-=======
 	if(Input.is_action_just_pressed("move_right")): #DEBUG: adds 1 iron ingot to inventory (delete later)
 		var rng = RandomNumberGenerator.new()
 		pickup(Item.new(rng.randi_range(1,4),1),0)
->>>>>>> 9acbe04a5cd8e5fd9d7f7aa133ca5a6c2364b667
 		
 	if(itemInHand != null): #Displays the item in hand if an item is being held
 		$heldItem.position = get_local_mouse_position()
